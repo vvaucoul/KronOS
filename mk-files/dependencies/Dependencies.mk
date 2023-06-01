b62aa2f01a2cb97dc05f655885cb148c86b7b784
vvaucoul <vvaucoul@student.42.fr>
2023-06-01 16:56:45 +0200

    Update

--- mk-files/dependencies/Dependencies.mk
+++ mk-files/dependencies/Dependencies.mk
@@ -6,7 +6,7 @@
 #    By: vvaucoul <vvaucoul@student.42.fr>          +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2022/08/29 20:39:54 by vvaucoul          #+#    #+#              #
-#    Updated: 2023/06/01 16:52:37 by vvaucoul         ###   ########.fr        #
+#    Updated: 2023/06/01 16:56:41 by vvaucoul         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -25,5 +25,5 @@ install-dependencies:
 	@sudo apt-get install ccache -y
 	@sudo apt-get install clang -y
 	@sudo apt install qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virt-manager -y
-	@sudo usermod -aG libvirt $USER
+	@sudo usermod -aG libvirt $(USER)
 	@sudo apt-get update && sudo apt-get upgrade -y
